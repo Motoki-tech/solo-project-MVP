abab4b72eb3428a2d8b55190d16ac561ea483b54
Motoki-tech <motokimizuno.0107@gmail.com>
2021-12-15 15:06:15 +0900

    add: checkboxを増やした

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -5,11 +5,43 @@ export default function Checkbox(props) {
     <div id='menu-box'>
       <label className='checkbox'>
         Elixir
-        <input type='checkbox' id='checkbox' onClick={() => {}} />
+        <input
+          type='checkbox'
+          id='checkbox'
+          onClick={(e) => {
+            props.setCurrentView("Elixir");
+          }}
+        />
       </label>
       <label className='checkbox'>
         ERNIE BALL
-        <input type='checkbox' id='checkbox' onClick={() => {}} />
+        <input
+          type='checkbox'
+          id='checkbox'
+          onClick={() => {
+            props.setCurrentView("ERNIE BALL");
+          }}
+        />
+      </label>
+      <label className='checkbox'>
+        DAddario
+        <input
+          type='checkbox'
+          id='checkbox'
+          onClick={(e) => {
+            props.setCurrentView("Daddario");
+          }}
+        />
+      </label>
+      <label className='checkbox'>
+        SIT
+        <input
+          type='checkbox'
+          id='checkbox'
+          onClick={(e) => {
+            props.setCurrentView("SIT");
+          }}
+        />
       </label>
     </div>
   );
