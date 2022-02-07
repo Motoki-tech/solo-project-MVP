c1c59f7bce44c0fccbd5c270796e47d49aa3f3d1
Motoki-tech <81059230+Motoki-tech@users.noreply.github.com>
2022-02-08 01:18:25 +0900

    サーバー接続

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
+const res = require("express/lib/response");
 const app = require("./app");
 const db = require("./knex");
 
-const port = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
-(async () => {
-  try {
-    console.log("Running migrations...");
-    await db.migrate.latest();
+// (async () => {
+//   try {
+//     console.log("Running migrations...");
+//     await db.migrate.latest();
 
-    console.log("Starting express...");
-    app.listen(port, () => {
-      console.log(`🎉App listening on port ${port}!🎉`);
-    });
-  } catch (err) {
-    console.error("Error starting app!", err);
-    process.exit(-1);
-  }
-})();
+//     console.log("Starting express...");
+//     app.listen(port, () => {
+//       console.log(`🎉App listening on port ${port}!🎉`);
+//     });
+//   } catch (err) {
+//     console.error("Error starting app!", err);
+//     process.exit(-1);
+//   }
+// })();
+app.listen(PORT, () => {
+  console.log(`${PORT}sucess!!!!`);
+});
