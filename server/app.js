397a908daaa1e9c811603dc0bb8fa9d496eb1664
Motoki-tech <motokimizuno.0107@gmail.com>
2022-02-08 01:18:25 +0900

    サーバー接続

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
-//sever/app.js
 const express = require("express");
 const app = express();
-const db = require("./knex");
+// const db = require("./knex");
 
 app.use(express.json());
 
-app.get("/api/string", async (req, res) => {
-  const notes = await db("string").select();
-  res.status(200).send(notes);
-});
+// app.get("/api/string", async (req, res) => {
+//   const notes = await db("string").select();
+//   res.status(200).send(notes);
+// });
 
 module.exports = app;
